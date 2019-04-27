b815cad4564a76dbc6017ac2ba5296dac5f909ee
Bryce Larson <blarson@saltstack.com>
2019-04-26 18:12:06 -0600

    test building with sphinx 2.0.1 and latexmk

--- build_env.sh
+++ build_env.sh
@@ -3,8 +3,8 @@
 ln -nsf /usr/share/zoneinfo/Etc/UTC /etc/localtime
 apt-get update
 apt-get install -y python3-pip lftp git openssh-client fontconfig make
-pip3 install sphinx==1.5.6
-git clone --depth 1 -b ${DOC_BRANCH} https://github.com/saltstack/salt /root/salt
+pip3 install sphinx==2.0.1
+git clone --depth 1 -b ${DOC_BRANCH} https://github.com/bryceml/salt /root/salt
 mkdir -p /usr/share/fonts/truetype
 mkdir -p /usr/share/fonts/opentype
 cp -rfv builddocs/files/fonts/truetype/* /usr/share/fonts/truetype/
