20274c950dfda7ca7d8ad9876abe9213637eca2e
Bryce Larson <blarson@saltstack.com>
2019-04-29 21:42:04 -0600

    test pdf validity after build

--- build_pdf.sh
+++ build_pdf.sh
@@ -6,5 +6,13 @@
 # and will change your timezone if you run it on your system.
 ln -nsf /usr/share/zoneinfo/Etc/UTC /etc/localtime
 
-apt-get install -y texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended texlive-xetex latexmk
+apt-get install -y texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended texlive-xetex latexmk poppler-utils
 make -C /root/salt/doc pdf
+
+echo "### Head 200 lines of log ###"
+head -n 200 /root/salt/doc/_build/latex/Salt.log
+echo "### Tail 200 lines of log ###"
+tail -n 200 /root/salt/doc/_build/latex/Salt.log
+
+echo "### Testing PDF ###"
+pdfinfo /root/salt/doc/_build/latex/Salt.pdf
